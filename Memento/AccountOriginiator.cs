a9df4fe5ce5a2979d0bb37b18d231671a4ab8b3d
Anil <anil.tallapudi@gmail.com>
2018-03-05 23:49:28 -0800

    Adding Memento Pattern

---
+++ Memento/AccountOriginiator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    public class AccountOriginator
+    {
+        public AccountState CurrentState { get; set; } 
+        public string AccountHolder { get; set; }
+
+        public Memento CreateMemento() {
+            return new Memento(CurrentState, AccountHolder);
+        }
+
+        public void SetMemento(Memento state) {
+            CurrentState = state.AccountState;
+            AccountHolder = state.AccountGuardian;
+        }
+    }
+
+    public class AccountState {
+        public int CurrentAmmount { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
--- Memento/Program.cs
+++ Memento/Program.cs
@@ -10,6 +10,39 @@ namespace Memento
     {
         static void Main(string[] args)
         {
+            AccountOriginator originator = new AccountOriginator();
+            originator.AccountHolder = "Anil";
+            originator.CurrentState = new AccountState { CurrentAmmount = 5000, LastUpdated = new DateTime(2009, 01, 01) };
+            CareTaker ct = new CareTaker();
+            ct.Memento = originator.CreateMemento();
+
+
+            Console.WriteLine("Application state Before change");
+            Console.WriteLine(originator.AccountHolder);
+            Console.WriteLine(originator.CurrentState.CurrentAmmount);
+            Console.WriteLine(originator.CurrentState.LastUpdated);
+
+            originator.AccountHolder = "Tester";
+            originator.CurrentState.CurrentAmmount = 200;
+            originator.CurrentState.LastUpdated = DateTime.Now;
+
+
+            Console.WriteLine("Application state Post change");
+            Console.WriteLine(originator.AccountHolder);
+            Console.WriteLine(originator.CurrentState.CurrentAmmount);
+            Console.WriteLine(originator.CurrentState.LastUpdated);
+
+            originator.SetMemento(ct.Memento);
+            Console.WriteLine("Application state Restored");
+            Console.WriteLine(originator.AccountHolder);
+            Console.WriteLine(originator.CurrentState.CurrentAmmount);
+            Console.WriteLine(originator.CurrentState.LastUpdated);
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
