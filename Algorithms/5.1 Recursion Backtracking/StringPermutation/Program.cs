9c0f6228c322aec1f3576738903b1a2abe60763c
Anil <anil.tallapudi@gmail.com>
2019-09-29 17:17:17 -0500

    Adding the logic

--- Algorithms/5.1 Recursion Backtracking/StringPermutation/Program.cs
+++ Algorithms/5.1 Recursion Backtracking/StringPermutation/Program.cs
@@ -8,16 +8,22 @@ namespace StringPermutation
     Algorithm:
     Using this method we we be using recursion to find permutations of a given string
     Logic:
-    1. Take the String into arry and have the count of the element
-    2. Use An array and fill it as we traverse to the next subcall of Find Permutation
-    2. As you Branch from the String Pointer, decrement its count
+    1. Get the string into Array
+    2. Generate a string[]  and Count[] which keeps the char and their count
+    3. For genrating result we need List<String> to store result Set, 
+        a char array of same length which will be filled to ensure we are using all strings
+    4. Start permutation with [-,-,-][A,B,C] , [2,1,1] , level 0 , result[]
+    5. Keep Track of of Level in recursion to fill in the Result String
+    6. Before Each Recursion Call, reduce the count of the char used to pass on to next Level
+    7. On Resolution of Recursion, increment the count of the char (Restoring the original Precodition)
+    
     */
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(args[0]);
+            //Console.WriteLine(args[0]);
 
             StringPermutationUtil sp = new StringPermutationUtil();
             var result = sp.permute("AABC".ToCharArray());
@@ -46,22 +52,28 @@ namespace StringPermutation
             char[] str = new char[countMap.Count];
             int[] count = new int[countMap.Count];
             int index = 0;
+            // 2. Generate a Map of Char -> Count
             foreach (KeyValuePair<char, int> entry in countMap)
             {
                 str[index] = entry.Key;
                 count[index] = entry.Value;
                 index++;
             }
+            // Container for List of Permutations
             List<string> resultList = new List<string>();
             char[] result = new char[input.Length];
-            permuteUtil(str, count, result, 0, resultList);
+            //  4. Start permutation with [-,-,-][A,B,C] , [2,1,1] , level 0 , result[]
+            permutateString(str, count, result, 0, resultList);
             return resultList;
         }
 
-        public void permuteUtil(char[] str, int[] count, char[] result, int level, List<String> resultList)
+        public void permutateString(char[] str, int[] count, char[] result, int level, List<String> resultList)
         {
+            // 5. Keep Track of of Level in recursion to fill in the Result String
             if (level == result.Length)
             {
+                Console.WriteLine(level);
+                Console.WriteLine(result.Length);
                 resultList.Add(new String(result));
                 return;
             }
@@ -73,8 +85,10 @@ namespace StringPermutation
                     continue;
                 }
                 result[level] = str[i];
+                // 6. Before Each Recursion Call, reduce the count of the char used to pass on to next Level
                 count[i]--;
-                permuteUtil(str, count, result, level + 1, resultList);
+                permutateString(str, count, result, level + 1, resultList);
+                // 7. On Resolution of Recursion, increment the count of the char (Restoring the original Precodition)
                 count[i]++;
             }
         }
