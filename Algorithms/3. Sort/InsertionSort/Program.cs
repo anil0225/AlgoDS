99aeee929b27f8b1776ebdfb79fe29c93824af53
Anil <anil.tallapudi@gmail.com>
2019-10-06 22:21:48 -0500

    Insertion Sort

---
+++ Algorithms/3. Sort/InsertionSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    // Approach
+    // We need to pick an item starting from 2nd position and compare it to the left of the position and keep the item in its ordered place
+    // 1. We need to iterate the given Array (Outer loop)
+    // 2. Start iterating from 2nd postion
+    // 3. We need a pointer to point the elements to left 
+    // 4. Since we need to insert the element at position and move all to right, lets pull the item to be compared out of array
+    // 5. Now start moving the elements of the array in the left to right until value to be place  < element at the pointer
+    // 6. Decrement the pointer till we satisfy 5 and iterate
+    // 7. Once satisfied , place the element at the pointed location
+    class Program
+    {
+        public class InsertionSort
+        {
+            public int[] sort(int[] input)
+            {
+                int pointer = 0;
+
+                for (int i = 1; i < input.Length; i++)
+                {
+                    int valuetoPlace = input[i];
+                    pointer = i - 1;
+                    while (pointer >= 0 && input[pointer] > valuetoPlace)
+                    {
+                        input[pointer + 1] = input[pointer];
+                        pointer--;
+                    }
+                    input[pointer + 1] = valuetoPlace;
+                }
+                return input;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time for Insertion Sort");
+            int[] input = { 2, 4, 1, 9, 3, 66, 33, 11 };
+            InsertionSort sorter = new InsertionSort();
+            var result = sorter.sort(input);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write($"{input[i]},");
+            }
+
+        }
+    }
+}
---
+++ Algorithms/3. Sort/MergeSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MergeSort
+{
+    class Program
+    {
+        public class MergeSort
+        {
+            public int[] Sort(int[] input, int start, int end)
+            {
+                int midPointer = end - start / 2;
+                Sort(input, start, midPointer);
+                Sort(input, midPointer + 1, end);
+                Merge(input, start, end);
+            }
+
+            public int[] Merge(int[] input, int start1, int end1, int start2, int end2)
+            {
+                int rightPointer = start1;
+                int leftPointer = start2;
+
+                while(start1 > end1 && start2 > end2){
+                    if(input[rightPointer] > input[leftPointer]){
+                        rightPointer++;
+                    }
+                    else {
+                        
+                    }
+                    if(input[rightPointer]  < input[leftPointer]){
+                        int temp = input[leftPointer];
+                        input[leftPointer] = input[rightPointer];
+                        input[leftPointer] =  input[rightPointer];
+                        leftPointer ++ ;
+                        rightPointer ++                       
+                    }
+                }
+
+                return input;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets start to do the Merge Sort");
+            int[] input = { 2, 4, 1, 9, 3, 66, 33, 11 };
+            MergeSort sorter = new MergeSort();
+            var result = sorter.sort(input, 0, input.Length - 1);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write($"{input[i]},");
+            }
+
+        }
+    }
+}
