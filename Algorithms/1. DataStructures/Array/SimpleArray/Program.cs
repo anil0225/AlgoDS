01b7e1d16c30208cd698cb90236542c2c086e690
Anil <anil.tallapudi@gmail.com>
2019-10-07 22:06:10 -0500

    Sum of SubArray

--- Algorithms/1. DataStructures/Array/SimpleArray/Program.cs
+++ Algorithms/1. DataStructures/Array/SimpleArray/Program.cs
@@ -4,11 +4,15 @@ namespace SimpleArray
 {
     class Program
     {
+        static void PrintAarray(int[] input)
+        {
+
+        }
         static void Main(string[] args)
         {
             int[] myArray = new int[10];
             Console.WriteLine("Hello World!");
-            
+
             Console.WriteLine(myArray.Length);
             for (int i = 0; i < myArray.Length; i++)
             {
@@ -22,7 +26,16 @@ namespace SimpleArray
                 Console.WriteLine(myArray[i]);
             }
 
-            
+
+            Console.WriteLine("Initialize Array");
+            int[] newArray = { 1, 2, 3, 4 };
+
+            Console.WriteLine("Multidimensinal Array , specify by Row/Column");
+            int[,] multiArray = new int[4, 3];
+            multiArray = new int[,] { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
+            Console.WriteLine("Multidimensinal Array");
+
+
         }
     }
 }
---
+++ Algorithms/1. DataStructures/Array/SumOfSubArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SumOfSubArray
+{
+    // Find Continuous array whose sum is as given
+    // Add all the the ints start from first pointer till the sum >= required sum
+    // If the sum is equal print Array
+    // If sum >  remove the first interger and proceed  next
+    class Program
+    {
+        static void PrintArray(int[] input, int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                Console.Write($"{input[i]},");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] input = { 2, 0, 1, 3, 5, 6 };
+            int requiredSum = 6;
+
+            int currentArraySum = 0;
+
+            int arrayPointer = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                while (currentArraySum < requiredSum && arrayPointer < input.Length)
+                {
+                    currentArraySum += input[arrayPointer];
+                    arrayPointer++;
+                }
+                if (currentArraySum == requiredSum)
+                {
+                    PrintArray(input, i, arrayPointer);
+                }
+
+                currentArraySum -= input[i];
+
+            }
+        }
+    }
+}
