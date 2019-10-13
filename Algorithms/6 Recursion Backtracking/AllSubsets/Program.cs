be14155fcab891c900d0a4e4f372d595fa98c2ef
Anil <anil.tallapudi@gmail.com>
2019-10-12 19:29:33 -0500

    Backtrack

--- Algorithms/6 Recursion Backtracking/AllSubsets/Program.cs
+++ Algorithms/6 Recursion Backtracking/AllSubsets/Program.cs
@@ -9,40 +9,32 @@ namespace AllSubsets
         public void Subset(int[] nums, List<List<int>> resultContainer, List<int> tempList, int start)
         {
 
-            var temp =  new List<int>(tempList);
-            PrintList(temp);
-            resultContainer.Add(new List<int>(temp));
+            var temp = new List<int>(tempList);
+            // PrintList(temp);
+            resultContainer.Add(temp);
+            PrintResultSet(resultContainer);
             for (int i = start; i < nums.Length; i++)
             {
                 temp.Add(nums[i]);
                 Subset(nums, resultContainer, temp, i + 1);
-                temp.Remove(temp.Count - 1);
+                temp.RemoveAt(temp.Count - 1);
             }
         }
 
         public void PrintList(List<int> tempList)
         {
             Console.WriteLine("Printing Temp List [");
+            
             foreach (var item in tempList)
             {
                 Console.Write($"{item},");
             }
             Console.WriteLine("]");
         }
-    }
-    class Program
-    {
-        static void Main(string[] args)
+
+        public void PrintResultSet(List<List<int>> resultContainer)
         {
-            Console.WriteLine("Time for Doing Full Backtrack");
-            int[] input = { 1,2,3 };
-            Console.WriteLine("Hello World!");
-            List<List<int>> resultContainer = new List<List<int>>();
-            BackTracker backTracker = new BackTracker();
-            backTracker.Subset(input, resultContainer, new List<int>(), 0);
-
-            Console.WriteLine($"Total sets {resultContainer.Count}");
-
+            Console.WriteLine("Printing Result Container");
             foreach (var item in resultContainer)
             {
                 Console.Write("[");
@@ -52,6 +44,19 @@ namespace AllSubsets
                 }
                 Console.WriteLine("]");
             }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time for Doing Full Backtrack");
+            int[] input = { 1, 2, 3 };
+            Array.Sort(input);
+            Console.WriteLine("Hello World!");
+            List<List<int>> resultContainer = new List<List<int>>();
+            BackTracker backTracker = new BackTracker();
+            backTracker.Subset(input, resultContainer, new List<int>(), 0);
         }
 
 
