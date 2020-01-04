86a5df6f2b2943bdb60cd41bb7fa5f06f8957e5c
Anil <anil.tallapudi@gmail.com>
2020-01-04 16:28:29 -0600

    Temp BTree Commit

---
+++ Algorithms/1. DataStructures/Trees/TreeDefinitions/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TreeDefinitions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Building Binary Tree");
+        }
+    }
+
+    public class BinaryTree<T>: ICollection<T> where T:IComparable
+    {
+
+        private BinaryTreeNode<T> head;
+        public BinaryTree(T rootValue){
+            head = new BinaryTreeNode<T>(rootValue);
+        }
+        public virtual BinaryTreeNode<T> Root
+        {
+            get { return head; }
+        }
+
+        public int GetSize(){
+
+        }
+
+        public void Add(T nodeValue){
+            
+        }
+
+
+        public void Remove(){
+            
+        }
+
+        public bool Contains(){
+
+        }
+        
+        private void InsertRecursive(){
+
+        }
+
+        private void InsertIterative(){
+
+        }
+
+        private bool TraverseInorder(T){
+
+        }
+
+        private bool TraversePreOrder(T) {}
+
+        private bool TraversalPostOrder(T) {
+            
+        }
+
+
+    }
+
+    public class BinaryTreeNode<T> where T:IComparable
+    {
+        T nodeValue;
+        public BinaryTreeNode(T value)
+        {
+            nodeValue = value;
+        }
+        public BinaryTreeNode<T> Right { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Parent { get; set; }
+        public T Value { get { return nodeValue; } }
+        public bool IsLeaf
+        {
+            get
+            {
+                return this.Right == null && this.Left == null;
+            }
+        }
+        
+
+    }
+}
